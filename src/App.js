69447d8c1b40cc96a1e034a4fc218b380bf9bff0
amit-aromatic <rahul.bhola.91@gmail.com>
2021-09-19 17:26:00 +0530

    login component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
-import './App.css';
+import Login from './components/Login/Login';
 
-function App() {
+function App()  {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="container-fluid">
+
+              <a className="navbar-brand" href="/"><img src={logo} width="30"  alt="logo" /></a>
+              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav">
+
+                </div>
+              </div>
+
+          </div>
+        </nav>
       </header>
+
+      <div className="row">
+        <div className="col col-md-6 offset-md-3">
+          <h1 className="text-center">
+            Welcome to Home page
+          </h1>
+        </div>
+      </div>
+
     </div>
   );
 }
